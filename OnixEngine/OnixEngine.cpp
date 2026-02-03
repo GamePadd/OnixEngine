468f157889ce11e8268c912233867dffaa18086b
GamePad <hejdjdjuduf@gmail.com>
2026-02-03 05:32:28 +0300

    Добавьте файлы проекта.

---
+++ OnixEngine/OnixEngine.cpp
@@ -0,0 +1,57 @@
+﻿#include "OnixEngine.h"
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void resize_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0,0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window,true);
+	}
+}
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(640, 480, "Main Window", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Can't create window!\n";
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, resize_callback);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 640, 480);
+
+	while (!glfwWindowShouldClose(window)) {
+		//input
+		processInput(window);
+
+		//render
+
+		glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//eof render
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
